56b9c656d3e133f6dc83e08aeb56eb6a91ad7f43
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-02-07 22:33:02 +0100

    delete-junk script no longer deleting pdf

--- delete_junk.bat
+++ delete_junk.bat
@@ -1,3 +1,3 @@
 echo off
 rem the next line deletes only files and NOT directories.
-del "*.lol" del "*.tdo" del "*.pdf" del "*.lof" del "*.xml" del "*.acn" del "*.xdy" del "*.idx" del "*.bcf" del "*.bbl" del "*.blg" del "*.log" del "*.out" del "*.toc" del "*.aux" del "*.lox" del "*.tps" del "*.nlo" del "*.gz"
+del "*.lol" del "*.tdo" del "*.lof" del "*.xml" del "*.acn" del "*.xdy" del "*.idx" del "*.bcf" del "*.bbl" del "*.blg" del "*.log" del "*.out" del "*.toc" del "*.aux" del "*.lox" del "*.tps" del "*.nlo" del "*.gz"

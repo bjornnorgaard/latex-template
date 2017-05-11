59b9be910487039585a8f50a472c9db85b3070f5
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-11 18:51:14 +0200

    Updated delete_junk.bat script. Now each cmd is on seperate lines. Also added line for deleting .dvi files

--- template/delete_junk.bat
+++ template/delete_junk.bat
@@ -1,3 +1,23 @@
 echo off
-rem the next line deletes only files and NOT directories.
-del "*.lol" del "*.tdo" del "*.lof" del "*.xml" del "*.acn" del "*.xdy" del "*.idx" del "*.bcf" del "*.bbl" del "*.blg" del "*.log" del "*.out" del "*.toc" del "*.aux" del "*.lox" del "*.tps" del "*.nlo" del "*.gz"
+rem the next line
+deletes only files and NOT directories.
+
+del "*.lol"
+del "*.tdo"
+del "*.lof"
+del "*.xml"
+del "*.acn"
+del "*.xdy"
+del "*.idx"
+del "*.bcf"
+del "*.blg"
+del "*.bbl"
+del "*.log"
+del "*.out"
+del "*.toc"
+del "*.aux"
+del "*.lox"
+del "*.tps"
+del "*.nlo"
+del "*.dvi"
+del "*.gz"
